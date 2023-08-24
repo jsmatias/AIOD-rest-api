8603435c42e72b42c5409d80e7b93a65b1ab69ce
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-08-24 21:56:47 +0200

    Fill the enums with default values

--- connectors/fill-examples.sh
+++ connectors/fill-examples.sh
@@ -51,5 +51,33 @@ python3 connectors/synchronization.py \
 # Enums
 
 python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorApplicationArea \
+  -w /opt/connectors/data/enum/application_area
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorEducationalResourceType \
+  -w /opt/connectors/data/enum/educational_resource_type
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorEventMode \
+  -w /opt/connectors/data/enum/event_mode
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorEventStatus \
+  -w /opt/connectors/data/enum/event_status
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorLanguage \
+  -w /opt/connectors/data/enum/language
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorLicense \
+  -w /opt/connectors/data/enum/license
+
+python3 connectors/synchronization.py \
+  -c connectors.example.enum.EnumConnectorOrganisationType \
+  -w /opt/connectors/data/enum/organisation_type
+
+python3 connectors/synchronization.py \
   -c connectors.example.enum.EnumConnectorStatus \
   -w /opt/connectors/data/enum/status
