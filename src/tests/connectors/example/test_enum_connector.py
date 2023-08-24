0fd027b107a5405427f1d9f315114492d6302873
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-08-24 22:00:50 +0200

    Cleanup

--- src/tests/connectors/example/test_enum_connector.py
+++ src/tests/connectors/example/test_enum_connector.py
@@ -1,20 +1,7 @@
-from typing import TYPE_CHECKING
-
-import pytest
-
 from connectors.example.enum import EnumConnectorStatus
 
-if TYPE_CHECKING:
-    from connectors.example.example_connector import ExampleConnector  # noqa:F401
 
-enum_connectors = {"status": EnumConnectorStatus()}
-
-
-@pytest.mark.parametrize(
-    "datatype",
-    ["status"],
-)
-def test_fetch_happy_path(datatype: str):
-    connector = enum_connectors[datatype]
+def test_fetch_happy_path():
+    connector = EnumConnectorStatus()
     resources = list(connector.fetch())
-    assert len(resources) >= 1
+    assert set(resources) == {"published", "draft", "rejected"}
