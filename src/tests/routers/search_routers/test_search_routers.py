4e19b1c2e846e967c67fe2a5e5319c0e57dd6af0
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-11-28 16:39:17 +0100

    Feature/elastic search db performance (#207)
    * Search router: get all identifiers in a single call to the db, to speedup
    
    * Simplified the search unittests, by avoiding code duplication

--- src/tests/routers/search_routers/test_search_routers.py
+++ src/tests/routers/search_routers/test_search_routers.py
@@ -1,30 +1,20 @@
-import os
 import json
-import pytest
-
 from unittest.mock import Mock
 
+import pytest
 from elasticsearch import Elasticsearch
 from starlette.testclient import TestClient
 
+import routers.search_routers as sr
+from authentication import keycloak_openid
 from routers.search_routers.elasticsearch import ElasticsearchSingleton
 from tests.testutils.paths import path_test_resources
-import routers.search_routers as sr
 
 
 @pytest.mark.parametrize("search_router", sr.router_list)
 def test_search_happy_path(client: TestClient, search_router):
-    """Tests the search router"""
-    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
-    ElasticsearchSingleton().patch(mocked_elasticsearch)
+    mock_elasticsearch(filename_mock=f"{search_router.es_index}_search.json")
 
-    resources_path = os.path.join(path_test_resources(), "elasticsearch")
-    resource_file = f"{search_router.es_index}_search.json"
-    mocked_file = os.path.join(resources_path, resource_file)
-    with open(mocked_file, "r") as f:
-        mocked_results = json.load(f)
-
-    mocked_elasticsearch.search = Mock(return_value=mocked_results)
     search_service = f"/search/{search_router.resource_name_plural}/v1"
     params = {"search_query": "description", "get_all": False}
     response = client.get(search_service, params=params)
@@ -45,19 +35,47 @@ def test_search_happy_path(client: TestClient, search_router):
         assert resource[field]
 
 
+def test_search_happy_path_get_all(client: TestClient, mocked_privileged_token: Mock):
+    keycloak_openid.userinfo = mocked_privileged_token
+    mock_elasticsearch(filename_mock="event_search.json")
+
+    body = {"name": "A name.", "keyword": ["keyword1", "keyword2"]}  # keywords not indexed by ES
+
+    response = client.post("/events/v1", json=body, headers={"Authorization": "Fake token"})
+    response.raise_for_status()
+
+    search_service = "/search/events/v1"
+    params = {"search_query": "description", "get_all": True}
+    response = client.get(search_service, params=params)
+
+    assert response.status_code == 200, response.json()
+    resource = response.json()["resources"][0]
+
+    assert resource["identifier"] == 1
+    assert resource["name"] == "A name."
+    assert resource["aiod_entry"]["status"] == "draft"
+    assert set(resource["keyword"]) == {"keyword1", "keyword2"}
+
+
+def test_search_get_all_not_found_in_db(client: TestClient):
+    mock_elasticsearch(filename_mock="event_search.json")
+
+    search_service = "/search/events/v1"
+    params = {"search_query": "description", "get_all": True}
+    response = client.get(search_service, params=params)
+
+    assert response.status_code == 404, response.json()
+    assert (
+        response.json()["detail"]
+        == "Some resources, with identifiers 1, could not be found in the database."
+    )
+
+
 @pytest.mark.parametrize("search_router", sr.router_list)
 def test_search_bad_platform(client: TestClient, search_router):
     """Tests the search router bad platform error"""
-    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
-    ElasticsearchSingleton().patch(mocked_elasticsearch)
+    mock_elasticsearch(filename_mock=f"{search_router.es_index}_search.json")
 
-    resources_path = os.path.join(path_test_resources(), "elasticsearch")
-    resource_file = f"{search_router.es_index}_search.json"
-    mocked_file = os.path.join(resources_path, resource_file)
-    with open(mocked_file, "r") as f:
-        mocked_results = json.load(f)
-
-    mocked_elasticsearch.search = Mock(return_value=mocked_results)
     search_service = f"/search/{search_router.resource_name_plural}/v1"
     params = {"search_query": "description", "platforms": ["bad_platform"]}
     response = client.get(search_service, params=params)
@@ -70,16 +88,8 @@ def test_search_bad_platform(client: TestClient, search_router):
 @pytest.mark.parametrize("search_router", sr.router_list)
 def test_search_bad_fields(client: TestClient, search_router):
     """Tests the search router bad fields error"""
-    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
-    ElasticsearchSingleton().patch(mocked_elasticsearch)
+    mock_elasticsearch(filename_mock=f"{search_router.es_index}_search.json")
 
-    resources_path = os.path.join(path_test_resources(), "elasticsearch")
-    resource_file = f"{search_router.es_index}_search.json"
-    mocked_file = os.path.join(resources_path, resource_file)
-    with open(mocked_file, "r") as f:
-        mocked_results = json.load(f)
-
-    mocked_elasticsearch.search = Mock(return_value=mocked_results)
     search_service = f"/search/{search_router.resource_name_plural}/v1"
     params = {"search_query": "description", "search_fields": ["bad_field"]}
     response = client.get(search_service, params=params)
@@ -92,16 +102,8 @@ def test_search_bad_fields(client: TestClient, search_router):
 @pytest.mark.parametrize("search_router", sr.router_list)
 def test_search_bad_limit(client: TestClient, search_router):
     """Tests the search router bad fields error"""
-    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
-    ElasticsearchSingleton().patch(mocked_elasticsearch)
+    mock_elasticsearch(filename_mock=f"{search_router.es_index}_search.json")
 
-    resources_path = os.path.join(path_test_resources(), "elasticsearch")
-    resource_file = f"{search_router.es_index}_search.json"
-    mocked_file = os.path.join(resources_path, resource_file)
-    with open(mocked_file, "r") as f:
-        mocked_results = json.load(f)
-
-    mocked_elasticsearch.search = Mock(return_value=mocked_results)
     search_service = f"/search/{search_router.resource_name_plural}/v1"
     params = {"search_query": "description", "limit": 1001}
     response = client.get(search_service, params=params)
@@ -120,16 +122,8 @@ def test_search_bad_limit(client: TestClient, search_router):
 @pytest.mark.parametrize("search_router", sr.router_list)
 def test_search_bad_offset(client: TestClient, search_router):
     """Tests the search router bad fields error"""
-    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
-    ElasticsearchSingleton().patch(mocked_elasticsearch)
+    mock_elasticsearch(filename_mock=f"{search_router.es_index}_search.json")
 
-    resources_path = os.path.join(path_test_resources(), "elasticsearch")
-    resource_file = f"{search_router.es_index}_search.json"
-    mocked_file = os.path.join(resources_path, resource_file)
-    with open(mocked_file, "r") as f:
-        mocked_results = json.load(f)
-
-    mocked_elasticsearch.search = Mock(return_value=mocked_results)
     search_service = f"/search/{search_router.resource_name_plural}/v1"
     params = {"search_query": "description", "offset": -1}
     response = client.get(search_service, params=params)
@@ -143,3 +137,12 @@ def test_search_bad_offset(client: TestClient, search_router):
             "type": "value_error.number.not_ge",
         }
     ]
+
+
+def mock_elasticsearch(filename_mock: str):
+    with open(path_test_resources() / "elasticsearch" / filename_mock, "r") as f:
+        mocked_results = json.load(f)
+
+    mocked_elasticsearch = Elasticsearch("https://example.com:9200")
+    mocked_elasticsearch.search = Mock(return_value=mocked_results)
+    ElasticsearchSingleton().patch(mocked_elasticsearch)
