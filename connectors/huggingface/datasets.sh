2083e6d395b16a06cdb313040c070dd0714db6f7
Jean Matias <smatias.jean@gmail.com>
2024-02-06 21:26:52 +0000

    minor change on huggingface datasets.sh so the logs are not wiped upon restarting the docker container

--- connectors/huggingface/datasets.sh
+++ connectors/huggingface/datasets.sh
@@ -6,4 +6,4 @@ mkdir -p $WORK_DIR
 python3 connectors/synchronization.py \
   -c connectors.huggingface.huggingface_dataset_connector.HuggingFaceDatasetConnector \
   -w ${WORK_DIR} \
-  --save-every 100 > ${WORK_DIR}/connector.log 2>&1
+  --save-every 100 >> ${WORK_DIR}/connector.log 2>&1
