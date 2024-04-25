f9fbde5d275b2a043b436c3beea9ef57c278c6cd
Adrián <adrianalcoleamoreno@gmail.com>
2024-04-25 13:10:16 +0200

    Entity educational_resources added to search functionality

--- src/setup/logstash_setup/templates/sql_rm.py
+++ src/setup/logstash_setup/templates/sql_rm.py
@@ -1,4 +1,6 @@
-TEMPLATE_SQL_RM = """SELECT {{entity_name}}.identifier
+TEMPLATE_SQL_RM = """SELECT
+    {{entity_name}}.identifier,
+    {{entity_name}}.date_deleted
 FROM aiod.{{entity_name}}
 WHERE aiod.{{entity_name}}.date_deleted IS NOT NULL
 AND aiod.{{entity_name}}.date_deleted > :sql_last_value
--- src/setup/logstash_setup/templates/sql_sync.py
+++ src/setup/logstash_setup/templates/sql_sync.py
@@ -4,7 +4,7 @@ TEMPLATE_SQL_SYNC = """SELECT
     {{entity_name}}.platform,
     text.plain as 'description_plain',
     text.html as 'description_html',
-    aiod_entry.date_modified{{extra_fields}}
+    aiod_entry.date_modified as 'date_modified'{{extra_fields}}
 FROM aiod.{{entity_name}}
 INNER JOIN aiod.aiod_entry ON aiod.{{entity_name}}.aiod_entry_identifier=aiod.aiod_entry.identifier
 LEFT JOIN aiod.text ON aiod.{{entity_name}}.description_identifier=aiod.text.identifier
