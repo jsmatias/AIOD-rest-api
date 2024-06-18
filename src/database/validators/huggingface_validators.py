116ccd25b2c8bc0cc077ea7e8b14faf5f4871144
taniya-das <taniyatd30@gmail.com>
2024-06-18 16:46:46 +0200

    code cleanup

--- src/database/validators/huggingface_validators.py
+++ src/database/validators/huggingface_validators.py
@@ -1,10 +1,6 @@
 import re
 from huggingface_hub.utils import validate_repo_id
 
-REPO_ID_ILLEGAL_CHARACTERS = re.compile(r"[^0-9a-zA-Z-_./]+")
-MSG_PREFIX = "The platform_resource_identifier for HuggingFace should be a valid repo_id. "
-
-
 def throw_error_on_invalid_identifier(platform_resource_identifier: str):
     """
     Throw a ValueError on an invalid repository identifier.
@@ -20,4 +16,3 @@ def throw_error_on_invalid_identifier(platform_resource_identifier: str):
     """
     repo_id = platform_resource_identifier
     validate_repo_id(repo_id=repo_id)
-    
