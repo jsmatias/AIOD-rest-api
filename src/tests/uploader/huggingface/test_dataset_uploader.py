ddf45eac3b68d78ce56ec721d22eea1d7f9a347d
taniya-das <taniyatd30@gmail.com>
2024-06-18 16:34:20 +0200

    update tests as huggingface validator is updated

--- src/tests/uploader/huggingface/test_dataset_uploader.py
+++ src/tests/uploader/huggingface/test_dataset_uploader.py
@@ -196,8 +196,8 @@ ERROR_MSG_PREFIX = f"The platform_resource_identifier is invalid for {PlatformNa
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=(
                     ERROR_MSG_PREFIX
-                    + "The platform_resource_identifier for HuggingFace should be a valid repo_id. "
-                    "A repo_id should only contain [a-zA-Z0-9] or ”-”, ”_”, ”.”"
+                    + "Repo id must use alphanumeric chars or '-', '_', '.', '--' and '..' are forbidden, '-' and '.' cannot start or end the name, max length is 96: "
+                    "'user/Test name with ?'."
                 ),
             ),
         ),
@@ -221,10 +221,8 @@ ERROR_MSG_PREFIX = f"The platform_resource_identifier is invalid for {PlatformNa
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=(
                     ERROR_MSG_PREFIX
-                    + "The platform_resource_identifier for HuggingFace should be a valid repo_id. "
-                    "For new repositories, there should be a single forward slash in the repo_id "
-                    "(namespace/repo_name). Legacy repositories are without a namespace. This "
-                    "repo_id has too many forward slashes."
+                    + "Repo id must be in the form 'repo_name' or 'namespace/repo_name': 'user/data/set'. " 
+                    "Use `repo_type` argument if needed."
                 ),
             ),
         ),
@@ -235,8 +233,8 @@ ERROR_MSG_PREFIX = f"The platform_resource_identifier is invalid for {PlatformNa
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=(
                     ERROR_MSG_PREFIX
-                    + "The namespace (the first part of the platform_resource_identifier) should be"
-                    " equal to the username, but wrong-namespace != user."
+                    + "The namespace (the first part of the platform_resource_identifier) should be "
+                    "equal to the username, but wrong-namespace != user."
                 ),
             ),
         ),
@@ -247,8 +245,8 @@ ERROR_MSG_PREFIX = f"The platform_resource_identifier is invalid for {PlatformNa
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=(
                     ERROR_MSG_PREFIX
-                    + "The platform_resource_identifier for HuggingFace should be a valid repo_id. "
-                    "A repo_id should be between 1 and 96 characters."
+                    +"Repo id must use alphanumeric chars or '-', '_', '.', '--' and '..' are forbidden, '-' and '.' cannot start or end the name, max length is 96: "
+                    "'user/" + "a" * 200 + "'."
                 ),
             ),
         ),
@@ -261,6 +259,6 @@ def test_validate_repo_id(username: str, dataset_name: str, expected_error: HTTP
     else:
         with pytest.raises(type(expected_error)) as exception_info:
             huggingface_uploader._validate_repo_id(dataset_name, username)
-
+        print(exception_info.value.detail)
         assert exception_info.value.status_code == expected_error.status_code
         assert exception_info.value.detail == expected_error.detail
