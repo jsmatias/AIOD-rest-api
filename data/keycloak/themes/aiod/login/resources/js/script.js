5f6e8e38ef280250c285f7ed019821debe99ea67
mrorro <mrorro@users.noreply.github.com>
2024-07-08 20:17:30 +0200

    Add login template

--- data/keycloak/themes/aiod/login/resources/js/script.js
+++ data/keycloak/themes/aiod/login/resources/js/script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.getElementById('kc-form-toggle');
+  const kcFormWrapper = document.getElementById('kc-form-wrapper');
 
-    const class1 = document.getElementById("kc-content-wrapper");
-    const class2 = document.getElementById("kc-form");
-    const class3 = document.getElementById("kc-social-providers");
+  function toggleVisibility() {
+    kcFormWrapper.style.display = kcFormWrapper.style.display === 'block' ? 'none' : 'block';
+    toggleButton.classList.toggle('up');
+  }
 
-    class1.insertBefore(class3, class2);
+        // Add event listener for the click event
+        toggleButton.addEventListener('click', toggleVisibility);
 
-    // Move specific content from class3 to the beginning of class2
-    const hrElement = class3.querySelector('hr');
-    const h2Element = class3.querySelector('h2');
-
-    if (h2Element) {
-      class2.insertBefore(h2Element, class2.firstChild);
-    }
-      
-    if (hrElement) {
-      class2.insertBefore(hrElement, h2Element || class2.firstChild);
-    }
-
+        const inputError = document.getElementById('input-error');
+        if (inputError) {
+          // Call the function once when the content loads if the specific ID is present
+          toggleVisibility();
+        }
 });
 
