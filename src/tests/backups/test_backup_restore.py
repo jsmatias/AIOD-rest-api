2b14f5f190a235294bd8f5696d790421c7451a99
PGijsbers <p.gijsbers@tue.nl>
2024-12-10 15:33:57 +0200

    Remove parameter again

--- src/tests/backups/test_backup_restore.py
+++ src/tests/backups/test_backup_restore.py
@@ -149,8 +149,7 @@ def test_backup_happy_path():
             assert backup_file.exists()
 
 
-@pytest.mark.parametrize("_", range(100))
-def test_restore_happy_path(_, data_environment: DataEnvironment):
+def test_restore_happy_path(data_environment: DataEnvironment):
     """
     Test if the restore.sh script restores the files properly.
     Usage:
